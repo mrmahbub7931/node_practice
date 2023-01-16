ef5de00cce6a7528f3ccff4a73b6176b1f112260
Mahbubar Rahman <md.mahbub7931@gmail.com>
2023-01-16 19:35:55 +0600

    parsed url

--- raw_node/index.js
+++ raw_node/index.js
@@ -4,6 +4,7 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // module scaffolding
 const app = {};
@@ -25,6 +26,12 @@ app.createServer = () => {
 // hanlde request and response
 
 app.handleReqRes = (req, res) => {
+    // Request handle
+    // get the url and parse
+    const parsedUrl = url.parse(req.url,true);
+    const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g,'');
+    const method = req.method.toLowerCase();
+    console.log(trimmedPath,method);
     // Response handle
     res.end("Hello Programmers!");
 }
